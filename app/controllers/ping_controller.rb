4c84d4ec58974165982a23c89c8b598a1c5b2ca2
TangentDC <steyn.dc@gmail.com>
2014-10-07 21:16:31 +0200

    Poking around at rails

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,5 +1,12 @@
+require 'location'
+
 class PingController < ApplicationController
 	def index 
 		render :text => "pong"
+	end
+	def locationlist
+		@location= ::Location.new(20.2, 23.5, "FirstLocation", "This is a test location")
+		@location.name="TheUpdatedName"
+		render :json => @location
 	end
 end
