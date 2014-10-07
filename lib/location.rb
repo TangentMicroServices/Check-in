8d2ef609495acf20a57cba3d9f2063a8868468de
TangentDC <steyn.dc@gmail.com>
2014-10-07 21:16:53 +0200

    Poking around at rails

---
+++ lib/location.rb
@@ -0,0 +1,20 @@
+class Location
+	def initialize(lat, long, name, description)
+		@lat=lat
+		@long=long
+		@name=name
+		@description=description
+	end
+	def name
+    	@name
+  	end
+ 
+  	def name=(value)
+    	@name = value
+  	end
+
+#	Number lat
+#	Number long
+#	Characters name
+#	Characters description
+end
